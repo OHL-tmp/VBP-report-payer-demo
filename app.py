66a6b165bedfbe89ddebb1e24726b18bd1072577
rtaromax <rtaromax@gmail.com>
2020-05-29 10:21:46 +0800

    update

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import dash
 
 
 app = dash.Dash(__name__, url_base_pathname='/vbc-demo/launch/')
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
 
 server = app.server
 
