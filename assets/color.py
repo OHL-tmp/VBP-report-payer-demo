9b416d06964fa3f821ced70393f8298ebbe68a30
yanchen <rtaromax@gmail.com>
2021-06-03 18:20:41 +0800

    update

---
+++ assets/color.py
@@ -0,0 +1,16 @@
+grey1 = "#f7f7f7"
+grey2 = "#d6d6d6"
+grey3 = "#f1f1f1"
+
+blue1 = "#063ce1"
+blue2 = "#6984d6"
+blue3 = "#e3eaff"
+blue4 = "#dee6ff"
+
+yellow1 = "#fcb612"
+yellow2 = "#ffefc9"
+yellow3 = "#cc9000"
+yellow4 = "#fff1cf"
+yellow_light1 = "#fff6e0"
+
+cyan = "#00a3b9"
